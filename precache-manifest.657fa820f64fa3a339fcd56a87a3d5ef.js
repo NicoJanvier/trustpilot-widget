95b7832cb9318bcee21e4c5c0495b682a6089636
Nico <nicolas.janvier@tyresonthedrive.com>
2019-11-02 17:04:37 +0000

    Updates

---
+++ precache-manifest.657fa820f64fa3a339fcd56a87a3d5ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a0bd9cd1bdd73e158a53f0a066b3d6",
+    "url": "/Phyros64/trustpilot-widget/index.html"
+  },
+  {
+    "revision": "1c8f843b613754f8502b",
+    "url": "/Phyros64/trustpilot-widget/static/css/main.35ea4c30.chunk.css"
+  },
+  {
+    "revision": "3177e99831f976da1de7",
+    "url": "/Phyros64/trustpilot-widget/static/js/2.7cb08e4b.chunk.js"
+  },
+  {
+    "revision": "1c8f843b613754f8502b",
+    "url": "/Phyros64/trustpilot-widget/static/js/main.e285d409.chunk.js"
+  },
+  {
+    "revision": "09b2e1f776f6857e8e9a",
+    "url": "/Phyros64/trustpilot-widget/static/js/runtime-main.c2b70afc.js"
+  }
+]);
