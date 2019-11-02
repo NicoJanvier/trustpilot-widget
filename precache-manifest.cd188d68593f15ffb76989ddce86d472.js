927363f47985841d2cd99283df733b5898bac7ee
Nico <nicolas.janvier@tyresonthedrive.com>
2019-11-02 17:10:33 +0000

    Updates

---
+++ precache-manifest.cd188d68593f15ffb76989ddce86d472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f070a7cfbc3873591e6d81b682c5c681",
+    "url": "/trustpilot-widget/index.html"
+  },
+  {
+    "revision": "1c8f843b613754f8502b",
+    "url": "/trustpilot-widget/static/css/main.35ea4c30.chunk.css"
+  },
+  {
+    "revision": "3177e99831f976da1de7",
+    "url": "/trustpilot-widget/static/js/2.7cb08e4b.chunk.js"
+  },
+  {
+    "revision": "1c8f843b613754f8502b",
+    "url": "/trustpilot-widget/static/js/main.e285d409.chunk.js"
+  },
+  {
+    "revision": "64f5c9529f2e785e19da",
+    "url": "/trustpilot-widget/static/js/runtime-main.b8a60766.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Phyros64/trustpilot-widget/precache-manifest.657fa820f64fa3a339fcd56a87a3d5ef.js"
+  "/trustpilot-widget/precache-manifest.cd188d68593f15ffb76989ddce86d472.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Phyros64/trustpilot-widget/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trustpilot-widget/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
